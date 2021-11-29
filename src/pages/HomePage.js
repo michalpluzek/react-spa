8c21a21682bed41d4a4420aa49404f0123bd0ee4
Michał Płużek <michalpluzek@gmail.com>
2021-11-29 15:06:48 +0100

    Add article structure and styles

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,34 @@
 import React from "react";
 
+import Article from "../components/Article";
+
+const articles = [
+  {
+    id: 1,
+    title: "Czym jest teoria strun",
+    author: "Jan Nowak",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sed nesciunt obcaecati itaque, incidunt doloribus debitis qui modi perferendis earum, eaque deleniti quibusdam, odit corporis consectetur dolor quam officia nisi.",
+  },
+  {
+    id: 2,
+    title: "Czym jest paradoks fermiego",
+    author: "Jan Kowalski",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sed nesciunt obcaecati itaque, incidunt doloribus debitis qui modi perferendis earum, eaque deleniti quibusdam, odit corporis consectetur dolor quam officia nisi.",
+  },
+  {
+    id: 3,
+    title: "Ciemna materia a ciemna energia",
+    author: "Anna Kwiatkowska",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sed nesciunt obcaecati itaque, incidunt doloribus debitis qui modi perferendis earum, eaque deleniti quibusdam, odit corporis consectetur dolor quam officia nisi.",
+  },
+];
+
 const HomePage = () => {
-  return <div>Strona startowa</div>;
+  const artList = articles.map((article) => (
+    <Article key={article.id} {...article} />
+  ));
+
+  return <div className="home">{artList}</div>;
 };
 
 export default HomePage;
