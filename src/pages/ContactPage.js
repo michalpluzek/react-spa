273c05120080646f8729925563fbdd111813bfae
Michał Płużek <michalpluzek@gmail.com>
2021-11-29 15:19:18 +0100

    Add contact structure and styles

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,7 +1,40 @@
 import React from "react";
+import "../styles/ContactPage.css";
 
-const ContactPage = () => {
-  return <div>Strona kontaku</div>;
-};
+class ContactPage extends React.Component {
+  state = {
+    value: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    this.setState({
+      value: "",
+    });
+  };
+
+  render() {
+    return (
+      <div className="contact">
+        <form onSubmit={this.handleSubmit}>
+          <h3>Napisz do nas</h3>
+          <textarea
+            value={this.state.value}
+            onChange={this.handleChange}
+            placeholder="Wpisz wiadomość..."
+          ></textarea>
+          <button>Wyślij</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default ContactPage;
