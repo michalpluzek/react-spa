39edc4649e495b48308c1037c468989200248782
Michał Płużek <michalpluzek@gmail.com>
2021-11-29 13:49:57 +0100

    Add components, template to app and styles

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,9 +1,26 @@
 import React from "react";
 import "../styles/App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import Header from "./Header";
+import Navigation from "./Navigation";
+import Page from "./Page";
+import Footer from "./Footer";
 
 class App extends React.Component {
   render() {
-    return <div>React router</div>;
+    return (
+      <Router>
+        <div className="app">
+          <header>{<Header />}</header>
+          <main>
+            <aside>{<Navigation />}</aside>
+            <section className="page">{<Page />}</section>
+          </main>
+          <footer>{<Footer />}</footer>
+        </div>
+      </Router>
+    );
   }
 }
 
