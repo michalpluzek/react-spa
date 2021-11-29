0a9f0b06f91c80673b33f38fa57e0dfb51c20b6e
Michał Płużek <michalpluzek@gmail.com>
2021-11-29 14:40:49 +0100

    Add header structure and styles

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import "../styles/Header.css";
+
+import img1 from "../images/header1.jpg";
+import img2 from "../images/header2.jpg";
+import img3 from "../images/header3.jpg";
 
 const Header = () => {
-  return <div>Header </div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<img src={img1} alt="miasto" />} />
+        <Route path="products" element={<img src={img2} alt="miasto" />} />
+        <Route path="contact" element={<img src={img3} alt="miasto" />} />
+        <Route path="admin" element={<img src={img1} alt="miasto" />} />
+        <Route path="*" element={<img src={img2} alt="miasto" />} />
+      </Routes>
+    </>
+  );
 };
 
 export default Header;
